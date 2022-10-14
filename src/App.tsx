c62ee3076fd2a5b5cb7979ba4983a01f6b33b020
unknown <r.matos96@gmail.com>
2022-10-14 10:52:21 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './styles/main.css';
+
+export function App() {
+
+  return (
+    <>
+      <h1 className="font-bold text-2xl">Hello My Friend</h1>
+      
+      <button>
+        enviar
+      </button>
+    </>
+  )
+}
+
